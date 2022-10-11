282840a7f32ec0550113173a873ca5f01bc0a102
Anna Gorokhovskaya <anna.gorokhovskaya@gmail.com>
2022-10-11 17:38:59 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import wikipedia
+
+#у бота сначала кнопка start
+# потом 2 языка на выбор - рус и англ
+wikipedia.set_lang("ru")
+
+# потом выбор - поиск по ключевым словам или рандомная статья
+
+def wiki():
+  while True:
+    random_article = wikipedia.random()
+    page = wikipedia.page(random_article)
+    print(f"{random_article}")
+    print(wikipedia.summary(random_article))
+    print("Вы хотели бы прочитать эту статью? (да/нет). Чтобы закончить работу наберите стоп.")
+    ans = input("").lower()
+    if ans == "да":
+        url = page.url
+        print(url)
+        break
+    elif ans == "нет":
+        print("Может быть другую?")
+        continue
+    elif ans == "стоп":
+        print("Приходите еще!")
+        continue
+    else:
+        print("Ошибка ввода")
+        print("Вы хотели бы прочитать эту статью? (да/нет). Чтобы закончить работу наберите стоп.")
+        ans = input("").lower()
+        if ans == "да":
+            url = page.url
+            print(url)
+            break
+
+if __name__ == "__main__":
+  try:
+    wiki()
+  except wikipedia.exceptions.DisambiguationError as e:
+      print(e.options)
